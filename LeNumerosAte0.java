6b3cc7402fb40004417eafabc56412fa613a9e61
Sabrina Gonçalves <goncalvessabrina74@gmail.com>
2023-09-12 21:40:12 -0300

    ap(12/09): soma e media

--- LeNumerosAte0.java
+++ LeNumerosAte0.java
@@ -4,20 +4,28 @@ public class LeNumerosAte0 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int acumulada = 0;
+        int cont = 0;
 
-        System.out.print("Digite seus numeros \n\n0 encerra ");
+        System.out.print("Digite seus numeros, 0 encerra : ");
 
         int m = scanner.nextInt();
 
-        while (m !=0){
+        while (m !=0){ 
+            cont = cont + 1;
             acumulada = acumulada + m;
             System.out.print("proximo: " );
             m = scanner.nextInt();
-            
         }
 
-        System.out.println("obrigado, tamo junto! acumulada é : " + acumulada);
-
+        if (cont == 0){
+            System.out.println("nenhum valor foi digitado");
+        }
+        else {
+            System.out.println("obrigado, tamo junto! acumulada é : " + acumulada);
+            double media = (double)acumulada / cont;
+            System.out.println("media dos valores : " + media);
+        }
+        
         scanner.close();
     }    
 }
